a53402aeef8aabb0f0b72bfbdd45e28887933f10
Tobias Reich <tobias.reich.ich@gmail.com>
2020-05-10 14:34:29 +0200

    Update build

--- build.js
+++ build.js
@@ -1,10 +1,8 @@
 'use strict'
 
-const { writeFile } = require('fs')
-const { promisify } = require('util')
+const { writeFile } = require('fs').promises
 const js = require('rosid-handler-js')
 const sass = require('rosid-handler-sass')
-const save = promisify(writeFile)
 
 sass('src/styles/main.scss', {
 
@@ -12,7 +10,7 @@ sass('src/styles/main.scss', {
 
 }).then((data) => {
 
-	return save('dist/basicRotate.min.css', data)
+	return writeFile('dist/basicRotate.min.css', data)
 
 })
 
@@ -30,6 +28,6 @@ js('src/scripts/main.js', {
 
 }).then((data) => {
 
-	return save('dist/basicRotate.min.js', data)
+	return writeFile('dist/basicRotate.min.js', data)
 
 })
